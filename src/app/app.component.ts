06dd204377049e942afdc89d798913b17e046116
mifag <mifag92@rambler.ru>
2023-09-26 14:23:55 +0300

    Add birthday date.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'business-card';
+   title = 'business-card';
+
+   getAge = birthDate => Math.floor((new Date().valueOf() - new Date(birthDate).valueOf()) / 3.15576e+10)
 }
